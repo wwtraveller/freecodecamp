4cc6ed0e4982029af95d5fe16db67f38981019bf
wwtraveller <ashilimova@mail.ru>
2024-01-09 11:45:22 +0100

    Step 48 Move goTown function above goStore function. Then copy and paste the contents of the goStore function into the goTown function.

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -21,6 +21,16 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+function goTown() {
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innerText = "You enter the store.";
+}
 function goStore() {
     button1.innerText = "Buy 10 health (10 gold)";
     button2.innerText = "Buy weapon (30 gold)";
@@ -44,6 +54,4 @@ function buyWeapon() {
 
 }
 
-function goTown() {
 
-}
